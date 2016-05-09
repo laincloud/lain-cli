977437d9aef6e1b616716e0a972a75ae820ed787
lipan <panli7@creditease.cn>
2016-05-09 13:03:47 +0800

    depended resource and service procname should not be same

--- lain_cli/validate.py
+++ lain_cli/validate.py
@@ -1,13 +1,36 @@
 # -*- coding: utf-8 -*-
 import sys
-from utils import lain_yaml_data
+from utils import lain_yaml_data, lain_yaml
 from lain_sdk.util import error, info, warn
 from lain_sdk.yaml.validator import validate as sdk_validate
 
 
 def _validate():
+    if _exist_same_procname_for_depends():
+        return False, 'procname of the depended services and resources should be different'
+
     yaml_source_data = lain_yaml_data()
     return sdk_validate(yaml_source_data)
+
+
+def _exist_same_procname_for_depends():
+    yml = lain_yaml(ignore_prepare=True)
+
+    service_procs, resource_procs = [], []
+    if hasattr(yml, 'use_services'):
+        use_services = yml.use_services
+        for _, service_list in use_services.iteritems():
+            for service in service_list:
+                service_procs.append(service)
+
+    if hasattr(yml, 'use_resources'):
+        use_resources = yml.use_resources
+        for _, resource_props in use_resources.iteritems():
+            for resouce_service_proc_name in resource_props['services']:
+                resource_procs.append(resouce_service_proc_name)
+
+    commons = set(service_procs) | set(resource_procs)
+    return len(commons) < len(service_procs) + len(resource_procs)
 
 
 def validate_only_warning():
