f947b0112aa11cd7755a5dbac9c65542904fddbb
BaiJian <ericpai94@hotmail.com>
2016-05-30 17:29:50 +0800

    Enhance lain enter and support lain attach (#3)
    * Add attach sub-command
    
    * Add -t parameter support for lain enter/attach
    
    Close #laincloud/lain/36 and #laincloud/lain/59

--- lain_cli/attach.py
+++ lain_cli/attach.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 from argh.decorators import arg
 from entryclient import EntryClient
-from lain_sdk.util import error
+from lain_sdk.util import error, info
 from lain_cli.auth import SSOAccess, authorize_and_check
 from lain_cli.utils import check_phase, lain_yaml, get_domain
 
-import os
-
 
 @arg('phase', help="lain cluster phase id, can be added by lain config save")
-@arg('-t', '--target', help='The target appname to enter. If it\'s not set, it will be the appname of the working dir')
-def enter(phase, proc_name, instance_no, target=None):
+@arg('-t', '--target', help='The target appname to attach, if not set, will be the appname of the working dir')
+def attach(phase, proc_name, instance_no, target=None):
     """
     enter the container of specific proc
     """
@@ -21,16 +19,16 @@ def enter(phase, proc_name, instance_no, target=None):
     authorize_and_check(phase, appname)
     domain = get_domain(phase)
     access_token = SSOAccess.get_token(phase)
-
-    term_type = os.environ.get("TERM", "xterm")
-    endpoint = "wss://entry.%s/enter" % domain
+    endpoint = "wss://entry.%s/attach" % domain
     header_data = ["access-token: %s" % access_token,
                    "app-name: %s" % appname,
                    "proc-name: %s" % proc_name,
-                   "instance-no: %s" % instance_no,
-                   "term-type: %s" % term_type]
+                   "instance-no: %s" % instance_no]
     try:
         client = EntryClient(endpoint, header=header_data)
-        client.invoke_shell()
+        info("Start to attach the stdout/stderr of the container. Press <Ctrl+c> to stop...")
+        client.attach_container()
+    except KeyboardInterrupt:
+        pass
     except:
         error("Server stops the connection. Ask admin for help.")
