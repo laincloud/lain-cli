e41bba123efdb5fc941a1f460439dd9416b7a1a3
wenbinmeng <wenbinmeng@creditease.cn>
2017-07-03 12:07:14 +0800

    Added lain push notify auth

--- lain_cli/push.py
+++ lain_cli/push.py
@@ -5,6 +5,7 @@ import json
 from argh.decorators import arg
 
 from lain_sdk.util import error, warn, info
+from lain_cli.auth import SSOAccess, get_auth_header
 import lain_sdk.mydocker as docker
 from lain_cli.utils import check_phase, lain_yaml, get_domain, git_authors, git_commit_id
 
@@ -23,6 +24,7 @@ def push(phase):
         error("please git commit.")
         return None
     domain = get_domain(phase)
+
     registry = "registry.%s" % domain
     phase_meta_tag = docker.gen_image_name(
         yml.appname, 'meta', meta_version, registry)
@@ -36,26 +38,28 @@ def push(phase):
     else:
         info("Done lain push.")
         info("notifying lain push.")
-        last_commit_id = fetch_last_commit_id(domain, yml.appname)
+        access_token = SSOAccess.get_token(phase)
+        auth_header = get_auth_header(access_token)
+        last_commit_id = fetch_last_commit_id(domain, yml.appname, auth_header)
         if last_commit_id is not None:
-            notify_diffs(domain, yml.appname, last_commit_id)
+            notify_diffs(domain, yml.appname, last_commit_id, auth_header)
         else:
             warn("Notified Nothing!")
         info("Done notifying lain push.")
 
 
-def fetch_last_commit_id(domain, appname):
+def fetch_last_commit_id(domain, appname, auth_header):
     url = "http://console.%s/api/v1/repos/%s/details/" % (domain, appname)
     commitid_length = 40
     try:
-        resp = requests.request("GET", url, timeout=10)
+        resp = requests.request("GET", url, headers=auth_header, timeout=10)
         datas = resp.json()
         if resp.status_code == 404:
             warn('%s' % datas['msg'])
             return None
         elif resp.status_code < 200 or resp.status_code >= 400:
             warn('Fetch lastest meta version failed with error: %s' %
-                  datas['msg'])
+                 datas['msg'])
             return None
         details = datas['detail']
         meta_version = details.get('meta_version', '')
@@ -71,20 +75,22 @@ def fetch_last_commit_id(domain, appname):
     return None
 
 
-def notify_diffs(domain, appname, last_commit_id):
+def notify_diffs(domain, appname, last_commit_id, auth_header):
     current_git_commit = git_commit_id()
     if current_git_commit == last_commit_id:
         warn('nothing changed!')
         return
     unique_authors = git_authors(last_commit_id, 'HEAD')
     headers = {"Content-type": "application/json"}
+    headers.update(auth_header)
     url = "http://console.%s/api/v1/repos/%s/push/" % (domain, appname)
     body = {'authors': unique_authors}
     try:
-        resp = requests.request("POST", url, headers=headers, json=body, timeout=10)
+        resp = requests.request(
+            "POST", url, headers=headers, json=body, timeout=10)
         datas = resp.json()
         if resp.status_code < 200 or resp.status_code >= 400:
             warn('Notify lain push failed with error: %s' %
-                  datas['msg'])
+                 datas['msg'])
     except Exception as e:
         warn('Notify lain push failed with error: %s' % e)
